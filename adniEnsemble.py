dc2450366853e57160d0240919fe72da493c1715
kvsaboo <ksaboo2@illinois.edu>
2018-11-29 22:03:16 -0600

    code to run over different data modalities and perform cross-validation on them for different classifiers and store results

---
+++ adniEnsemble.py
@@ -0,0 +1,81 @@
+# basic modules
+import numpy as np
+import pandas as pd
+import scipy.io
+# learning model modules
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+# learning support modules
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+# learning metrics modules
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+
+
+
+def SensiSpeci(true_labels, predicted_labels):
+    confmat = confusion_matrix(true_labels, predicted_labels)
+    sensitivity = 100*confmat[1,1]/(confmat[1,0]+confmat[1,1]) # TP/(FN+TP)
+    specificity = 100*confmat[0,0]/(confmat[0,0]+confmat[0,1]) # TN/(TN+FP)
+    return sensitivity, specificity
+
+
+
+def gridSearchWrapper(classifier_name, paramdict, num_cv, train_X, train_Y, test_X, test_Y):
+
+    # select and train the model
+    if classifier_name.lower() == 'logistic_regression':
+        mlmodel = LogisticRegressionCV(Cs=paramdict, cv=num_cv, max_iter=500)
+        mlmodel.fit(train_X, train_Y)
+    
+    elif classifier_name.lower() == 'svm':
+        grid_classifier = GridSearchCV(estimator=SVC(), 
+                                       param_grid=paramdict, cv=num_cv, verbose=1)
+        # train model
+        grid_classifier.fit(train_X, train_Y)
+        mlmodel = grid_classifier.best_estimator_ # select best model
+        
+    elif classifier_name.lower() == 'random_forest':
+        # grid search for finding best hyper parameters        
+        grid_classifier = GridSearchCV(estimator=RandomForestClassifier(),
+                                       param_grid=paramdict, cv=num_cv, verbose=1)
+        # train model
+        grid_classifier.fit(train_X, train_Y)
+        mlmodel = grid_classifier.best_estimator_ # select best model
+        
+    else:
+        print("Method not found")
+        return 0
+    
+    # test model - test data
+    sensitivity_test, specificity_test = SensiSpeci(test_Y, mlmodel.predict(test_X))
+    accuracy_test = 100*mlmodel.score(test_X, test_Y)
+    f1_test = f1_score(test_Y, mlmodel.predict(test_X))
+    
+    # test model - training data
+    sensitivity_train, specificity_train = SensiSpeci(train_Y, mlmodel.predict(train_X))
+    accuracy_train = 100*mlmodel.score(train_X, train_Y)
+    f1_train = f1_score(train_Y, mlmodel.predict(train_X))
+    
+    # prepare dict to return
+    test_score_dict = {'sensitivity':sensitivity_test, 'specificity':specificity_test, 'accuracy':accuracy_test, 'f1':f1_test}
+    train_score_dict = {'sensitivity':sensitivity_train, 'specificity':specificity_train, 'accuracy':accuracy_train, 'f1':f1_train}
+    
+    return {'model': mlmodel, 'test_score':test_score_dict, 'train_score':train_score_dict}
+
+
+
+def ttSplitWithGridSearch(classifier_name, paramdict, num_cv, datadf, feature_name, label_name, train_fraction, random_seed):
+    # prepare training and test data with appropriate features and randomization
+    train_X, test_X, train_Y, test_Y = train_test_split(
+        datadf[feature_name], datadf[label_name], train_size=train_fraction, test_size=1-train_fraction,
+        random_state=random_seed, shuffle=True)
+    
+    # preform grid search to choose best model
+    grid_search_out = gridSearchWrapper(classifier_name, paramdict, num_cv, 
+                              train_X, train_Y, test_X, test_Y)
+    
+    # return best model and its performance
+    return grid_search_out
---
+++ cvForModalities.py
@@ -0,0 +1,65 @@
+# basic modules
+import numpy as np
+import pandas as pd
+import scipy.io
+import pickle
+import adniEnsemble as adens # module with customized code
+
+# load ADNI dataset and do pre-processing
+adnifulldf = pd.read_csv("../Dataset/ADNI/adnitable.csv")
+adnifulldf = adnifulldf.fillna(-1)
+adnifulldf.DX = adnifulldf.DX.apply(lambda x: 'CN' if x==1 else ('MCI' if x==2 else ('AD' if x==3 else -1)))
+
+## initilizations
+# different classifiers and parameters
+clf_list = ['logistic_regression','svm','random_forest']
+svc_param_grid = {'C':np.logspace(-2,2,5), 'kernel':['linear'], 'degree':[2], 'gamma':[0.1, 1, 10]}
+rf_param_grid = {'n_estimators':np.arange(10,21,dtype=int), 'max_depth':np.arange(3,7,dtype=int)}
+clf_to_param_dict = {'logistic_regression':20, 'svm':svc_param_grid, 'random_forest':rf_param_grid}
+# general params for all classifiers
+train_frac = 0.8
+gs_num_cv = 4
+outer_num_cv = 10
+# data related initizations
+modality_list = ['amyloid','csf','fdg','pet']
+factors = ['Gender','Educ','Age','APOE']
+clinical_vars = ['Gender','Educ','Age','APOE','MMSE','DX']
+dx_stages = ['CN', 'AD']
+# saving results related initiazations
+save_dir_path = '../Dataset/ProcessedFiles/'
+
+# run through all data modalities
+for modality in modality_list:
+    print("Data modality is: " + modality)
+    # find columns in entire dataframe related to modality of interest
+    modality_vars = [fieldname for fieldname in adnifulldf.columns if modality in fieldname] 
+    # create separate dataframe for modality of interest; keep only CN and AD and remove non-APOE subjects
+    modalitydf = adnifulldf.loc[(adnifulldf["DX"].isin(dx_stages)) & (adnifulldf["APOE"] != -1) 
+                                & (adnifulldf[modality+'_01'] != -1), clinical_vars+modality_vars].copy()
+    modalitydf['DX_bin'] = np.where(modalitydf["DX"]=="CN", 0, 1) # CN:0 , AD: 1; required for classification
+
+    # whether or not to include factors like Gender, age and education in classifier
+    for include_factors in [0,1]:
+        if include_factors == 0: # do not include 'factors' in the features
+            features = modality_vars
+        else: # include "factors" on the features
+            features = [item for sublist in [factors, modality_vars] for item in sublist]
+        
+        # run through all classification models
+        for clf_name in clf_list:
+            print("Classifier is: " + clf_name)
+            grid_out_list = []
+            # perform outer_num_cv-fold cross validation
+            for cvn in range(0, outer_num_cv):
+                # perform train-test split on data and grid search for hyperparameters on training data
+                grid_out = adens.ttSplitWithGridSearch(clf_name, clf_to_param_dict[clf_name], gs_num_cv, 
+                                              modalitydf, features, 'DX_bin', train_frac, cvn) 
+                grid_out_list.append(grid_out)
+
+            # file name to store results in
+            if include_factors == 0: # do not include 'factors' in the features
+                save_fname = save_dir_path + clf_name +'_'+ modality+'_cv.pckl'
+            else: # include "factors" on the features
+                save_fname = save_dir_path + clf_name +'_'+ modality+'_factors_cv.pckl'
+            
+            pickle.dump(grid_out_list, open(save_fname, 'wb'))
